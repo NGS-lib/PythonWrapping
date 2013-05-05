596181d2a003ce91c3002b03de31f1b3440c2169
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2013-05-04 22:34:29 -0400

    Added most of uBasicNGS set/get functions and tested them briefly
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- foo.cpp
+++ foo.cpp
@@ -37,9 +37,115 @@ extern "C" {
 		return (char*) (basic->getChr().c_str());
 	}
 
+	char* getStrand(uBasicNGS* basic) {
+		cout << "Before calling uBasicNGS->getStrand()" << endl;
+		if (basic->getStrand() == StrandDir::FORWARD) {
+			string strand_string("FORWARD");
+			return (char*)(strand_string.c_str());
+		}
+		else {
+			string strand_string("REVERSE");
+			return (char*)(strand_string.c_str());
+		}
+	}
+
+	long int getStart(uBasicNGS* basic) {
+		cout << "Before calling uBasicNGS->getStart()" << endl;
+		return basic->getStart();
+	}
+
+	long int getEnd(uBasicNGS* basic) {
+		cout << "Before calling uBasicNGS->getEnd()" << endl;
+		return basic->getEnd();
+	}
+
+	long int getLength(uBasicNGS* basic) {
+		cout << "Before calling uBasicNGS->getLength()" << endl;
+		return basic->getLength();
+	}
+
+	float getScore(uBasicNGS* basic) {
+		cout << "Before calling uBasicNGS->getScore" << endl;
+		return basic->getScore();
+	}
+
+	float getScorePosition(uBasicNGS* basic, int position) {
+		cout << "Before calling uBasicNGS->getScore (position)" << endl;
+		return basic->getScore(position);
+	}
+
+	int getScoreCount(uBasicNGS* basic) {
+		cout << "Before calling uBasicNGS->getScoreCount" << endl;
+		return basic->getScoreCount();
+	}
+
+	bool isReverse(uBasicNGS* basic) {
+		cout << "Before calling uBasicNGS->isReverse" << endl;
+		return basic->isReverse();
+	}
+
 	void setChr(uBasicNGS* basic, char* chr) {
 		cout << "Before calling uBasicNGS->setChr()" << endl;
 		basic->setChr(chr);
+	}
+
+	void setStrand(uBasicNGS* basic, char* strand) {
+		cout << "Before calling uBasicNGS constructor" << endl;
+		string strand_string(strand);
+		if (strand_string == "+" || strand_string == "forward" || strand_string == "FORWARD") {
+			basic->setStrand(StrandDir::FORWARD);
+		}
+		else if (strand_string == "-" || strand_string == "reverse" || strand_string == "REVERSE") {
+			basic->setStrand(StrandDir::REVERSE);
+		}
+		else {
+			throw logic_error("Invalid strand type");
+		}
+	}
+
+	void setStart(uBasicNGS* basic, long int start) {
+		cout << "Before calling uBasicNGS setStart" << endl;
+		basic->setStart(start);
+	}
+
+	void setEnd(uBasicNGS* basic, long int end) {
+		cout << "Before calling uBasicNGS setEnd" << endl;
+		basic->setEnd(end);
+	}
+
+	void setStartEnd(uBasicNGS* basic, long int start, long int end) {
+		cout << "Before calling uBasicNGS setStartEnd" << endl;
+		basic->setStartEnd(start, end);
+	}
+
+	void setScore(uBasicNGS* basic, float score) {
+		cout << "Before calling uBasicNGS setScore" << endl;
+		basic->setScore(score);
+	}
+
+	void setScorePosition(uBasicNGS* basic, float score, int position) {
+		cout << "Before calling uBasicNGS setScore (position)" << endl;
+		basic->setScore(score, position);
+	}
+
+	void extendSite(uBasicNGS* basic, long int extend) {
+		cout << "Before calling uBasicNGS extendSite" << endl;
+		basic->extendSite(extend);
+	}
+
+	void extendSiteLeftRight(uBasicNGS* basic, long int extendLeft, long int extendRight) {
+		cout << "Before calling uBasicNGS extendSite (left, right)" << endl;
+		basic->extendSite(extendLeft, extendRight);
+	}
+
+	void trimSite(uBasicNGS* basic, long int trim) {
+		cout << "Before calling uBasicNGS trimSite" << endl;
+		basic->trimSite(trim);
+	}
+
+	void trimSiteLeftRight(uBasicNGS* basic, long int trimLeft, long int trimRight) {
+		cout << "Before calling uBasicNGS trimSite (left, right)" << endl;
+		basic->trimSite(trimLeft, trimRight);
 	}
 
 } // End of extern "C"
--- fooWrapper.py
+++ fooWrapper.py
@@ -3,8 +3,13 @@
 # author: Charles Joly Beauparlant
 # 2013-05-01
 
+import ctypes
 from ctypes import *
 lib = cdll.LoadLibrary('./libfoo.so')
+
+# Tests with restype
+lib.getScore.restype = ctypes.c_float
+lib.getScorePosition.restype = ctypes.c_float
 
 class Foo(object):
 	def __init__(self, filename, filetype):
@@ -20,8 +25,62 @@ class Basic(object):
 	def getChr(self):
 		return c_char_p(lib.getChr(self.obj)).value
 
+	def getStrand(self):
+		return c_char_p(lib.getStrand(self.obj)).value
+
+	def getStart(self):
+		return lib.getStart(self.obj)
+
+	def getEnd(self):
+		return lib.getEnd(self.obj)
+
+	def getLength(self):
+		return lib.getLength(self.obj)
+
+	def getScore(self, position=None):
+		if position is None:
+			return lib.getScore(self.obj)
+		else:
+			return lib.getScorePosition(self.obj, position)
+
+	def getScoreCount(self):
+		return lib.getScoreCount(self.obj)
+
+	def isReverse(self):
+		return c_bool(lib.isReverse(self.obj))
+
 	def setChr(self, chr):
 		lib.setChr(self.obj, chr)
+
+	def setStrand(self, strand):
+		lib.setStrand(self.obj, strand)
+
+	def setStart(self, start):
+		lib.setStart(self.obj, start)
+
+	def setEnd(self, end):
+		lib.setEnd(self.obj, end)
+
+	def setStartEnd(self, start, end):
+		lib.setStartEnd(self.obj, start, end)
+
+	def setScore(self, score, position=None):
+		if position is None:
+			lib.setScore(self.obj, c_float(score))
+		else:
+			lib.setScorePosition(self.obj, c_float(score), position)
+
+	def extendSite(self, extendLeft, extendRight=None):
+		if extendRight is None:
+			lib.extendSite(self.obj, extendLeft)
+		else:
+			lib.extendSiteLeftRight(self.obj, extendLeft, extendRight)
+
+	def trimSite(self, trimLeft, trimRight=None):
+		if trimRight is None:
+			lib.trimSite(self.obj, trimLeft)
+		else:
+			lib.trimSiteLeftRight(self.obj, trimLeft, trimRight)
 
 # Tests with the parser
 f = Foo("test.bed", "BED")
@@ -32,6 +91,60 @@ else:
 
 # Tests with the uBasicNGS
 b = Basic("chr1", 100, 200)
+
 print b.getChr()
 b.setChr("chr2")
 print b.getChr()
+
+print b.getStrand()
+b.setStrand("-")
+print b.getStrand()
+
+print "**** Start/End/Length tests with set/get/extend/trim"
+print "Base Values"
+print "Start: " + str(b.getStart())
+print "End: " + str(b.getEnd())
+print "Length: " + str(b.getLength())
+print "setStart(50), setEnd(250)"
+b.setStart(50)
+b.setEnd(250)
+print "Start: " + str(b.getStart())
+print "End: " + str(b.getEnd())
+print "Length: " + str(b.getLength())
+print"setStartEnd(75,125)"
+b.setStartEnd(75,125)
+print "Start: " + str(b.getStart())
+print "End: " + str(b.getEnd())
+print "Length: " + str(b.getLength())
+print "extendSite(10)"
+b.extendSite(10)
+print "Start: " + str(b.getStart())
+print "End: " + str(b.getEnd())
+print "Length: " + str(b.getLength())
+print "trimSite(30)"
+b.trimSite(30)
+print "Start: " + str(b.getStart())
+print "End: " + str(b.getEnd())
+print "Length: " + str(b.getLength())
+print "extendSite(10,20)"
+b.extendSite(10,20)
+print "Start: " + str(b.getStart())
+print "End: " + str(b.getEnd())
+print "Length: " + str(b.getLength())
+print "trimSite(5,10)"
+b.trimSite(5,10)
+print "Start: " + str(b.getStart())
+print "End: " + str(b.getEnd())
+print "Length: " + str(b.getLength())
+
+print "**** Score tests"
+#print "Score: " + str(b.getScore())
+print "ScoreCount: " + str(b.getScoreCount())
+print "setScore(100.2)"
+b.setScore(100.3)
+print "Score: " + str(b.getScore())
+print "ScoreCount: " + str(b.getScoreCount())
+b.setScore(233.24, 1)
+print "Score(0): " + str(b.getScore(0))
+print "Score(1): " + str(b.getScore(1))
+print "ScoreCount: " + str(b.getScoreCount())
