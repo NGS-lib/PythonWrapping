7e2dcf730770e8669b9b48cef60bac707aae34ef
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2013-05-08 10:41:46 -0400

    Moved python wrapping of uBasicNGS functions to uBasicNGS_wrapping.py
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- fooWrapper.py
+++ fooWrapper.py
@@ -3,8 +3,8 @@
 # author: Charles Joly Beauparlant
 # 2013-05-01
 
-import ctypes
 from ctypes import *
+from uBasicNGS_wrapper import *
 lib = cdll.LoadLibrary('./libfoo.so')
 
 # Tests with restype
@@ -17,93 +17,6 @@ class Foo(object):
 
 	def eof(self):
 		return c_bool(lib.eof(self.obj))
-
-class Basic(object):
-	def __init__(self, chr="", start=1, end=1, strand="FORWARD"):
-		self.obj = lib.New_basic(chr, start, end, strand)
-
-	def __del__(self):
-		lib.delete_basic(self.obj)
-
-	def getChr(self):
-		return c_char_p(lib.getChr(self.obj)).value
-
-	def getStrand(self):
-		return c_char_p(lib.getStrand(self.obj)).value
-
-	def getStart(self):
-		return lib.getStart(self.obj)
-
-	def getEnd(self):
-		return lib.getEnd(self.obj)
-
-	def getLength(self):
-		return lib.getLength(self.obj)
-
-	def getScore(self, position=None):
-		if position is None:
-			return lib.getScore(self.obj)
-		else:
-			return lib.getScorePosition(self.obj, position)
-
-	def getScoreCount(self):
-		return lib.getScoreCount(self.obj)
-
-	def getScoreList(self):
-		scoreList = []
-		for i in range(0, self.getScoreCount()):
-			scoreList.append(self.getScore(i))
-		return scoreList
-
-	def isReverse(self):
-		return c_bool(lib.isReverse(self.obj))
-
-	def setChr(self, chr):
-		lib.setChr(self.obj, chr)
-
-	def setStrand(self, strand):
-		lib.setStrand(self.obj, strand)
-
-	def setStart(self, start):
-		lib.setStart(self.obj, start)
-
-	def setEnd(self, end):
-		lib.setEnd(self.obj, end)
-
-	def setStartEnd(self, start, end):
-		lib.setStartEnd(self.obj, start, end)
-
-	def setScore(self, score, position=None):
-		if position is None:
-			lib.setScore(self.obj, c_float(score))
-		else:
-			lib.setScorePosition(self.obj, c_float(score), position)
-
-	def extendSite(self, extendLeft, extendRight=None):
-		if extendRight is None:
-			lib.extendSite(self.obj, extendLeft)
-		else:
-			lib.extendSiteLeftRight(self.obj, extendLeft, extendRight)
-
-	def trimSite(self, trimLeft, trimRight=None):
-		if trimRight is None:
-			lib.trimSite(self.obj, trimLeft)
-		else:
-			lib.trimSiteLeftRight(self.obj, trimLeft, trimRight)
-
-	def doesOverlap(self, toCompare):
-		return c_bool(lib.doesOverlap(self.obj, toCompare.obj))
-
-	def returnOverlapping(self, toCompare):
-		toReturn = Basic()
-		toReturn.obj = lib.returnOverlapping(self.obj, toCompare.obj)
-		return toReturn
-
-	def returnMerge(self, toCompare):
-		toReturn = Basic()
-		toReturn.obj = lib.returnMerge(self.obj, toCompare.obj)
-		return toReturn
-
 
 # Tests with the parser
 f = Foo("test.bed", "BED")
